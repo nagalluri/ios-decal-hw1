04b5a1d416a645f896f62ee6a88527c7c954879c
Nag Alluri <nagalluri@berkeley.edu>
2015-09-16 23:46:21 -0700

    Finished HW1

--- debugging.playground/Contents.swift
+++ debugging.playground/Contents.swift
@@ -15,41 +15,41 @@ class Foo {
     var wordB : String!
     
     init (words: [String?]) {
-        wordA = words[0]?
-        wordB = words[1]?
+        wordA = words[0]!
+        wordB = words[1]!
     }
     
-//: [EXPLAIN YOUR ANSWER TO Q1 HERE]
+//: In this situation since words is an array of String Optionals and we are given that wordA and wordB can't be nil (because of !), so the proper way to unwrap the optional is to assert that it can't be nil.
     
 
     
 //: ## Q2: Variable Types and Function Types
 //: Why does the compiler dislike the for loop? Also, what should we return?
     
-    func arePalindromes(words: [String]) -> Bool! {
+   static func arePalindromes(words: [String]) -> Bool! {
         let reversedWords = words.map() {String($0.characters.reverse())}
-        var numElements = words.count
+        let numElements = words.count
         
-        for let i = 0; i < numElements; i++ {
+        for var i = 0; i < numElements; i++ {
             if words[i] != reversedWords[i] {
                 return false
             }
         }
         
-        return nil
+        return true
     }
     
-//: [EXPLAIN YOUR ANSWER TO Q2 HERE]
+//: The for loop should initialize i as a variable because i is being incremented. Similarly, numElements should be initialized using let since it remains constant throughout the function. The function should also return true instead of nil, because if the function passes through the for loop completely, than we know that the two words are palindromes. Also, when we have a bool as our return type, the return values are either true or false. I also changed the function to a static type so that the function calls can be made without creating an instance.
     
     
     
 //: ## Q3: More functions, and object initialization
 //: The method should be returning true or false -- what's wrong?
 //: Are we initializing the dictionary correctly?
-    func isAnagram(wordA: String, wordB: String) -> Bool? {
-        var countLetters : [Character : Int]
-        var lenA = wordA.characters.count
-        var lenB = wordB.characters.count
+    static func isAnagram(wordA: String, wordB: String) -> Bool? {
+        var countLetters = [Character : Int]()
+        let lenA = wordA.characters.count
+        let lenB = wordB.characters.count
         
         if lenA != lenB {
             return false
@@ -75,17 +75,17 @@ class Foo {
             }
         }
         
-        for (letter, count) in countLetters {
+        for (_,count) in countLetters {
             if count != 0 {
                 return false
             }
         }
         
-        return nil
+        return true
     }
 }
 
-//: [EXPLAIN YOUR ANSWER TO Q3 HERE]
+//: The dictionary was initialized incorrectly, I corrected the code above to use the proper syntax. Other than that, I fixed the for loop, I changed lenA and lenB to be constants instead of variables and changed the final return to true. I also made the function to a static type so that the function calls can be made without creating an instance.
 
 
 //: **Do not** change anything below.
